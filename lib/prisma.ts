0fb3725e0021d7e3713e32d3cf7bfebc3206a596
sushma-santra <sushma.santra@sportzinteractive.net>
2026-03-12 21:52:58 +0530

    Fix database connection

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,7 @@
 // lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
